8a14390b89cc5fc00f332b7eeadddfce54eac961
movies6158 <130030817+movies6158@users.noreply.github.com>
2023-04-06 14:19:19 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1680760696,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite3-sheet1.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"media/mixkit-cartoon-dazzle-hit-and-birds-746.m4a",
+		"media/mixkit-cartoon-dazzle-hit-and-birds-746.ogg",
+		"media/mixkit-achievement-bell-600.m4a",
+		"media/mixkit-achievement-bell-600.ogg",
+		"media/mixkit-losing-drums-2023.m4a",
+		"media/mixkit-losing-drums-2023.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
